ffc57a3ef35ce06fac6d6f2f7163f1e2f1d12634
hosornoc <hosornoc@gmail.com>
2020-10-06 22:23:17 -0500

    Ajustes a la llamada

--- app/src/main/java/com/cytophone/services/views/CallView.java
+++ app/src/main/java/com/cytophone/services/views/CallView.java
@@ -58,8 +58,8 @@ public class CallView extends AppCompatActivity {
         Function1<Integer, Unit> updateUI = new Function1<Integer, Unit>() {
             @Override
             public Unit invoke(Integer state) {
-               if( null == CallView.this._party &&
-                   OngoingCall.isCallActive() ) { // End call
+                // && OngoingCall.isCallActive()
+                if( null == CallView.this._party ) { // End call
                     OngoingCall.INSTANCE.reject();
                     OngoingCall.INSTANCE.hangup();
                 } else {  // Accept call and show caller info
